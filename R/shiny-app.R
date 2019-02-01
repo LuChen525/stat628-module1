7685f8fba5e9e751f0f2e4ef3632322475d2f841
Sam Waterbury <samwaterbury1@gmail.com>
2019-01-31 19:50:34 -0600

    Moved files

--- R/shiny-app.R
+++
@@ -1,8 +0,0 @@
-#' This file contains the code necessary to run the Shiny visualization.
-
-source('analysis.R')
-
-# This is the example that comes with Shiny
-runExample('01_hello', launch.browser = TRUE)
-
-# To do...
